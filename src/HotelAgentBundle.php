fb2e6d3afff0e022730dd6db7a298f82b402a200
github-ci <you@example.com>
2025-06-11 14:35:37 +0800

    fix

--- src/HotelAgentBundle.php
+++ src/HotelAgentBundle.php
@@ -3,7 +3,21 @@
 namespace Tourze\HotelAgentBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
+use Tourze\DoctrineUserBundle\DoctrineUserBundle;
+use Tourze\HotelContractBundle\HotelContractBundle;
+use Tourze\HotelProfileBundle\HotelProfileBundle;
 
-class HotelAgentBundle extends Bundle
+class HotelAgentBundle extends Bundle implements BundleDependencyInterface
 {
+    public static function getBundleDependencies(): array
+    {
+        return [
+            HotelProfileBundle::class => ['all' => true],
+            HotelContractBundle::class => ['all' => true],
+            DoctrineTimestampBundle::class => ['all' => true],
+            DoctrineUserBundle::class => ['all' => true],
+        ];
+    }
 }
